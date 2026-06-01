35b1210a9701628b50e9c8aec5e2d160801583e8
Gustavo Teodoro <109152947+gusteodoro04@users.noreply.github.com>
2026-06-01 19:53:29 -0300

    Create Project

---
+++ JavaHashEncadeado2026-main/src/CustomHashMap.java
@@ -0,0 +1,127 @@
+import java.util.LinkedList;
+
+public class CustomHashMap<T> {
+    private LinkedList<Dado<T>>[] tabela;
+    private int tamanho;
+    private int numElementos;
+    private int comparacoes;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    private static class Dado<T> {
+        long chave;
+        T valor;
+
+        Dado(long key, T value) {
+            this.chave = key;
+            this.valor = value;
+        }
+    }
+
+
+    public CustomHashMap(int tamanho) {
+        this.tamanho = tamanho;
+        tabela = new LinkedList[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            tabela[i] = new LinkedList<>();
+        }
+    }
+
+    public CustomHashMap() {
+        this.tamanho = 16;
+        tabela = new LinkedList[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            tabela[i] = new LinkedList<>();
+        }
+    }
+
+    private int funcaoHash(long chave) {
+        return (int) (chave % tamanho);
+    }
+
+    public void put(long key, T value) {
+        if ((double) numElementos / tamanho >= DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+
+        Dado<T> dado = new Dado<>(key, value);
+        int indice = funcaoHash(key);
+        tabela[indice].add(dado);
+        numElementos++;
+    }
+
+    public boolean containsKey(long key) {
+        int indice = funcaoHash(key);
+        for (Dado<T> item : tabela[indice]) {
+            if (item.chave == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remove(long key) {
+        int indice = funcaoHash(key);
+        for (Dado<T> item : tabela[indice]) {
+            if (item.chave == key) {
+                tabela[indice].remove(item);
+                numElementos--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T get(long key) {
+        int indice = funcaoHash(key);
+        for (Dado<T> item : tabela[indice]) {
+            if (item.chave == key) {
+                return item.valor;
+            }
+        }
+        return null;
+    }
+
+    public boolean replace(long key, T value) {
+        int indice = funcaoHash(key);
+        for (Dado<T> item : tabela[indice]) {
+            if (item.chave == key) {
+                item.valor = value;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void resize() {
+        int novoTamanho = tamanho * 2;
+        this.tamanho = novoTamanho;
+        LinkedList<Dado<T>>[] novaTabela = new LinkedList[novoTamanho];
+        for (int i = 0; i < novoTamanho; i++) {
+            novaTabela[i] = new LinkedList<>();
+        }
+
+        for (LinkedList<Dado<T>> lista : tabela) {
+            for (Dado<T> dado : lista) {
+                int novoIndice = funcaoHash(dado.chave);
+                novaTabela[novoIndice].add(dado);
+            }
+        }
+
+        this.tabela = novaTabela;
+        System.out.println("Tabela redimensionada para " + novoTamanho + " elementos.");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tamanho; i++) {
+            sb.append("[").append(i).append("]");
+            for (Dado<T> item : tabela[i]) {
+                sb.append(" -> ").append(item.chave).append(":").append(item.valor);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
